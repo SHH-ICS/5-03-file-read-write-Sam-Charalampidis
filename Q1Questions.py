1c6bf0b23fff8f6f7420154fea41fd4ff9c25164
Sam-Charalampidis <sam.charalampidis@stu.ocsb.ca>
2024-12-05 10:33:40 -0500

    finished

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,11 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
+question = input("Input question: ")
+a = input("Input answer a: ")
+b = input("Input answer b: ")
+c = input("Input answer c: ")
+d = input("Input answer d: ")
+e = input("Input correct answers letter: ")
+filehandle = open("questions.txt","w")
+filehandle.write(question+"\n"+a+"\n"+b+"\n"+c+"\n"+d+"\n"+e)
+filehandle.close()
--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,2 +1,20 @@
 # Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
 # The program will keep score of the number of questions answered correctly.
+filehandle = open("questions.txt","r")
+question = filehandle.readline()
+a = filehandle.readline()
+b = filehandle.readline()
+c = filehandle.readline()
+d = filehandle.readline()
+e = filehandle.readline()
+filehandle.close
+print(question)
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("d:",d)
+guess = input("Which letter? ")
+if guess != str(e):
+    print("ur loss sucka")
+elif guess == str(e):
+    print("u win sucka")
